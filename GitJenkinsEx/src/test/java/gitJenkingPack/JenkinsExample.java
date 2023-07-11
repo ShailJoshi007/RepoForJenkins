775d3748838e5d99d2a45df125f51492ae13fcb2
ShailJoshi007 <shailbala.joshi@gmail.com>
2023-07-11 22:31:47 +0530

    FirstCommit

---
+++ GitJenkinsEx/src/test/java/gitJenkingPack/JenkinsExample.java
@@ -0,0 +1,10 @@
+package gitJenkingPack;
+
+public class JenkinsExample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Pushing to Git then Build in Jenkins");
+
+	}
+
+}
